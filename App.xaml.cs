58ab5f9c9282e60504bde744ff855fdbf2d4c84c
wardliii <91539136+wardliii@users.noreply.github.com>
2023-10-09 15:45:06 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Hangman.Data;
+
+namespace Hangman
+{
+    public partial class App : Application
+    {
+        public static GameRepository GameRepository { get; private set; }
+
+        public App(GameRepository gameRepository)
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            GameRepository = gameRepository;
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 800;
+            const int newHeight = 900;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
+    }
+}
