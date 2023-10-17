2d5bc4749f8d7c1903b09b3fddbf784438b8357e
Allan Forsyth <40582311@live.napier.ac.uk>
2023-10-17 16:15:01 +0100

    Revert "extraneous old files removed"
    This reverts commit bbab53eab56e4fe6004b1429ff8375ddaff4cec8.

---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+
+namespace newchellangemarek
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GamePage()
+        {
+            // Arranges
+            int gameType = 1; // Set the type of game
+            GamePage gamePage = new GamePage(gameType);
+
+            // accessing the private CreateNewChallenge method
+            MethodInfo createNewChallengeMethod = typeof(GamePage).GetMethod("CreateNewChallenge", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // Act
+            createNewChallengeMethod.Invoke(gamePage, null);
+
+            // Assert
+            // Checks that a Word has been selected based on GameType (customize this based on your implementation)
+            Assert.NotNull(gamePage.easy);
+
+            // Check that Display has been reset (e.g., its count is zero)
+            Assert.Equal(0, gamePage.Display.Count);
+        }
+    }
+}
